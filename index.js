f0657d6541c7b868f532ed5b3e2cd583f70ee79f
ka <ka.kaosf@gmail.com>
2023-07-09 20:04:59 +0900

    Tweak

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
+const npub = "a6f1f450080b65ba75da8ac7328f91c94f8314b2cc4aa719c516852a29388f0b";
+const relay = "wss://yabu.me";
+
 const WebSocketClient = require("websocket").client;
 
 const client = new WebSocketClient();
 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
+client.on("connectFailed", (error) => {
+  console.log("Connect Error: " + error.toString());
 });
 
-client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log(message);
-        }
-    });
-
-    function send() {
-        if (connection.connected) {
-            connection.sendUTF(JSON.stringify(['REQ', 'my-sub', { authors: ["a6f1f450080b65ba75da8ac7328f91c94f8314b2cc4aa719c516852a29388f0b"] }]));
-            setTimeout(send, 1000);
-        }
+client.on("connect", (connection) => {
+  console.log("WebSocket Client Connected");
+  connection.on("error", (error) => {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on("close", () => {
+    console.log("echo-protocol Connection Closed");
+  });
+  connection.on("message", (message) => {
+    if (message.type === "utf8") {
+      console.log(message);
     }
-    send();
+  });
+
+  const send = () => {
+    if (connection.connected) {
+      connection.sendUTF(
+        JSON.stringify(["REQ", "my-sub", { authors: [npub] }])
+      );
+      setTimeout(send, 1000);
+    }
+  };
+  send();
 });
 
-client.connect("wss://yabu.me");
+client.connect(relay);
